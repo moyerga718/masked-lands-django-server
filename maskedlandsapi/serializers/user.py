a35eb78c9d562d4348fac1f600cbc66227b3eeca
Griffin Moyer <moyerga@outlook.com>
2022-10-13 10:10:43 -0500

    updated response for login and register, added user serializer

--- maskedlandsapi/serializers/user.py
+++ maskedlandsapi/serializers/user.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User
-from django.db import models
 from rest_framework import serializers
 
 class UsernameSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = User
         fields = ('username',)
